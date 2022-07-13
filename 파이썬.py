e4e94907d2a6e796ec0cf7104bd7ca3383deb24d
rhqemd333@naver.com <kwon su hun>
2022-07-13 22:23:32 +0900

    코드업

--- 파이썬.py
+++ 파이썬.py
@@ -1,11 +1,12 @@
-i = int(input("숫자입력"))
-n = int(input("n 을 입력하세요"))
-c = 0
-while i <= n:
-    c = c + i
-    i = i + 1
-    print(c)    
+#주어진 리스트가 반장 선거 투표 결과일 때 이여희의 총 득표수를 출력.
 
 
-    
 
+students = ['이영희', '김철수', '이영희', '조민지', '김철수', '조민지', '이영희', '이영희']
+result = 0
+box = 0
+
+for box in students:
+    if box == "이영희":
+        result = result + 1
+print(result)
